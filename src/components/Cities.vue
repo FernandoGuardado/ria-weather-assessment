83d4f2cb772ee0bbc610da774186eab2f5c13ce5
Fernando Guardado-Pérez <fernandoguardado01@gmail.com>
2025-04-24 01:32:43 -0600

    scroll city list on scroll

--- src/components/Cities.vue
+++ src/components/Cities.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
+import { watch } from 'vue';
 import type { City } from '../App.vue';
 import CityListItem from './CityListItem.vue';
 
-defineProps<{
+const { selectedCity } = defineProps<{
   cities: City[];
   selectedCity: City;
 }>();
@@ -15,11 +16,24 @@ const emit = defineEmits<{
 const handleCitySelect = (city: City) => {
   emit('update:selectedCity', city);
 }
+
+watch(() => selectedCity, () => {
+  setTimeout(() => {
+    const citiesList = document.querySelector('.cities-list');
+    if (citiesList) {
+      const left = citiesList.scrollWidth - citiesList.clientWidth;
+      citiesList.scrollTo({
+        left: left,
+        behavior: 'smooth',
+      });
+    }
+  }, 200);
+}, { immediate: false });
 </script>
 
 <template>
   <div id="cities">
-    <section>
+    <section class="cities-list">
       <CityListItem v-for="(city, index) in cities" :key="index" :city="city" :selectedCity="selectedCity.name"
         @update:selectedCity="handleCitySelect" :class="index === (cities.length - 1) && `last-item`" />
     </section>
